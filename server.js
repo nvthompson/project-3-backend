370c735b7b5ced08cbc8b82789238efb7a870b05
Timmy Do <timmy.do130@gmail.com>
2022-01-29 10:13:00 -0700

    set up MongoDB connection and made Schema

--- server.js
+++ server.js
@@ -10,8 +10,34 @@ require('dotenv').config()
 //expose our config variables
 const { PORT, MONGODB_URL } = process.env
 
+//MongoDB connection
+mongoose.connect(MONGODB_URL)
+
+//MongoDB event listeners
+const db = mongoose.connection;
+
+db
+    .on('connected',()=> console.log('Connected to MongoDB'))
+    .on('Disconnected',()=> console.log('Disconnected to MongoDB'))
+    .on('Error',(error)=> console.log('MongoDB had an Error' + error.message))
+
+//setting up a model for books
+const bookStoreSchema = new mongoose.Schema({
+    title: String,
+    img: String,
+    descript: String,
+    price: Number,
+    qty: Number
+}, {timestamps});
+
+
 //middleware
 app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'))
+
+
+//Routes
 
 app.get('/', (req, res)=>{
     res.send('Hello world')
